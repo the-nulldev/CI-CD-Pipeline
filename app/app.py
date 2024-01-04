7afd8d619c25827dce965d732a7d7df065d3ee93
the-null-dev <ephantus640@gmail.com>
2024-01-04 12:57:53 +0300

    Modified message

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! Our awesome app is running! And the WebHook too.'
+    return 'Hello World! Our awesome app is running! And the WebHook.'
 
 
 if __name__ == '__main__':
